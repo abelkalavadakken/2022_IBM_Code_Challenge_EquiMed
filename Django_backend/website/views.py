4550e68d9940a403eee505c734090265eb1d87f4
SuryaDasN <suryadasn2000@gmail.com>
2022-05-05 22:47:17 +0530

    second commit

--- Django_backend/website/views.py
+++ Django_backend/website/views.py
@@ -1,9 +1,19 @@
+from distutils.command.sdist import sdist
 from django.http import HttpResponse
 from django.shortcuts import render
+from Django_backend.settings import BASE_DIR
 from .forms import ImageForm
 from .models import Image
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
 
 # Create your views here.
+def mish(inputs):
+    return inputs * tf.math.tanh(tf.math.softplus(inputs))
+tf.keras.utils.get_custom_objects().update({'mish': mish})
 
 def home(request):
     if request.method == "POST":
@@ -15,18 +25,21 @@ def home(request):
     img = Image.objects.all()
     return render(request,'home.html')
 
+def analyse(request):
 
-def file(request):
     img = Image.objects.all()
     l = len(img)
     x = img[l-1]
-    return render(request,'file.html',{'x': x})
+    new_model = load_model(os.path.join(BASE_DIR, 'models/cataractclassifierV10.h5'))
+    new_model.summary()
+    image_folder = str(BASE_DIR) + x.photo.url.replace("/","\\")
+    # test_image = image.load_img(os.path.join(image_folder),target_size=(150,150))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    result = new_model.predict(test_image)
+    if result == 1:
+        return render(request,'result.html')
+    else :
+        return render(request,'noresult.html')
 
-def analyse(request):
     return render(request,'analyse.html')
-
-def result(request):
-    return render(request,'result.html')
-
-def noresult(request):
-    return render(request,'noresult.html')
